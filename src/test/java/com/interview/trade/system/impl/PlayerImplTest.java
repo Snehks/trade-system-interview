3c2fe1b1da86fe096039d1a1000a7d0a86d360fe
Sneh <snehtekriwal@gmail.com>
2023-09-16 14:43:42 +0100

    Added a failing test.

--- src/test/java/com/interview/trade/system/impl/PlayerImplTest.java
+++ src/test/java/com/interview/trade/system/impl/PlayerImplTest.java
@@ -1,7 +1,14 @@
 package com.interview.trade.system.impl;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerImplTest {
     //TODO : Write Tests Here and follow similar approach for other classes.
+
+    @Test
+    void failingTest() {
+        fail();
+    }
 }
